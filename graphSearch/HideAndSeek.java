8fc3502625d6155c214ff94d46652385ba29ed01
Choi si young <msc200353@naver.com>
2021-10-07 21:57:34 +0900

    update

--- graphSearch/HideAndSeek.java
+++ graphSearch/HideAndSeek.java
@@ -3,60 +3,57 @@ package graphSearch;
 import java.io.*;
 import java.util.*;
 
-// https://www.acmicpc.net/problem/2178
+// https://www.acmicpc.net/problem/1697
 
-public class SearchMiro {
+public class HideAndSeek {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M;
+    static int N, K;
     static String[] a;
-    static int[][] dist;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static boolean[] visit;
+    static int[] dist;
+    static int[] dir = { -1, 1, 2};
 
     static void input() {
         N = scan.nextInt();
-        M = scan.nextInt();
-        a = new String[N];
-        for (int i=0; i<N; i++) {
-            a[i] = scan.nextLine();
-        }
-        dist = new int[N][M];
+        K = scan.nextInt();
+        dist = new int[100005];
+        visit = new boolean[100005];
     }
-
-    static void bfs(int x, int y) {
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) {
-                dist[i][j] = -1;
-            }
-        }
-
+    
+    static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(x);
-        queue.add(y);
-        dist[x][y] = 1;
+        queue.add(N);
+        dist[N] = 0;
+        visit[N] = true;
 
         while (!queue.isEmpty()) {
-            x = queue.poll();
-            y = queue.poll();
-            for (int k=0; k<4; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (a[nx].charAt(ny) == '0') continue;
-                if (dist[nx][ny] != -1) continue;
+            int x = queue.poll();
 
-                queue.add(nx);
-                queue.add(ny);
-                dist[nx][ny] = dist[x][y] + 1;
+            for (int i=0; i<3; i++) {
+                int y=x;
+                if (i != 2) y += dir[i];
+                else y *= dir[i];
+
+                if (y >= 0 && y <= 100000 && !visit[y]) {
+                    queue.add(y);
+                    visit[y] = true;
+                    dist[y] = dist[x]+1;
+
+                    if (y == K) {
+                        queue.clear();
+                        return;
+                    }
+                }
             }
+            
         }
     }
 
     static void pro() {
-        bfs(0, 0);
-
-        System.out.println(dist[N-1][M-1]);
+        bfs();
+        System.out.println(dist[K]);
     }
 
     public static void main(String[] args) {
