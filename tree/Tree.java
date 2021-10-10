87b490e6705dc69f35f98e16568fed25c6b82b69
Choi si young <msc200353@naver.com>
2021-10-10 11:58:56 +0900

    update tests

---
+++ tree/Tree.java
@@ -0,0 +1,109 @@
+package tree;
+
+import java.io.*;
+import java.util.*;
+
+// https://www.acmicpc.net/problem/1068
+
+public class Tree {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, root, erased;
+    static ArrayList<Integer>[] child;
+    static int[] leaf;
+
+    static void input() {
+        N = scan.nextInt();
+        child = new ArrayList[N];
+        leaf = new int[N];
+        for (int i=0; i<N; i++) child[i] = new ArrayList<>();
+        for (int i=0; i<N; i++) {
+            int par = scan.nextInt();
+            if (par == -1) {
+                root = i;
+                continue;
+            }
+            child[par].add(i);
+        }
+        erased = scan.nextInt();
+    }
+
+    // dfs(x, par) := 정점 x 의 부모가 par 였고, Subtree(x) 의 leaf 개수를 세주는 함수
+    static void dfs(int x) {
+        if (child[x].isEmpty()) {
+            leaf[x] = 1;
+        }
+        for (int y: child[x]) {
+            dfs(y);
+            leaf[x] += leaf[y];
+        }
+    }
+
+    static void pro() {
+        // erased와 그의 부모 사이의 연결을 끊어주기
+        for (int i=0; i<N; i++) {
+            if (child[i].contains(erased)) {
+                child[i].remove(child[i].indexOf(erased));
+            }
+        }
+
+        if (root != erased) {
+            dfs(root);
+        }
+        System.out.println(leaf[root]);
+    }
+
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
