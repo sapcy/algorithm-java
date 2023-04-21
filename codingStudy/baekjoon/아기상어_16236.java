2695574a9c8acf1758863922f7d30582605cb02e
siyoung choi <sy.choi@okestro.com>
2023-04-21 18:31:44 +0900

    test add

--- codingStudy/baekjoon/아기상어_16236.java
+++ codingStudy/baekjoon/아기상어_16236.java
@@ -1,32 +1,37 @@
 package codingStudy.baekjoon;
 
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-// https://www.acmicpc.net/problem/16234
+// https://www.acmicpc.net/problem/16236
 
-public class 인구이동_16234 {
+public class 아기상어_16236 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, L, R, count;
+    static int N, dist, sharkSize, sharkX, sharkY;
     static int[] dx = { 1, 0, -1, 0};
     static int[] dy = { 0, 1, 0, -1};
     static int[][] board;
     static boolean[][] visited;
-    static Queue<Nation> bfsQueue = new LinkedList<>();
-    static Queue<Nation> targetNations = new LinkedList<>();
+    static Queue<Node> bfsQueue = new LinkedList<>();
+    static Queue<Node> targetNodes = new LinkedList<>();
 
     static void input() {
         N = scan.nextInt();
-        L = scan.nextInt();
-        R = scan.nextInt();
+        sharkSize = 2;
         board = new int[N][N];
         visited = new boolean[N][N];
 
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
                 board[i][j] = scan.nextInt();
+                if (board[i][j] == 9) {
+                    sharkX = i;
+                    sharkY = j;
+                }
             }
         }
 
@@ -38,21 +43,8 @@ public class 인구이동_16234 {
             boolean isMoved = false;
             visited = new boolean[N][N];
 
-            for (int i=0; i<N; i++) {
-                for (int j=0; j<N; j++) {
-                    if (visited[i][j]) continue;
+            search(sharkX, sharkY);
 
-                    int sectionTotal = search(i, j);
-                    if (!targetNations.isEmpty()) {
-                        int avg = sectionTotal / targetNations.size();
-                        while (!targetNations.isEmpty()) {
-                            Nation nation = targetNations.poll();
-                            board[nation.x][nation.y] = avg;
-                        }
-                        isMoved = true;
-                    }
-                }
-            }
 
             // 한번도 이동이 없었으면 끝
             if (!isMoved) break;
@@ -64,14 +56,12 @@ public class 인구이동_16234 {
     }
 
     static int search(int x, int y) {
-        int sectionTotal = 0;
-        bfsQueue.add(new Nation(x, y));
-        targetNations.add(new Nation(x, y));
-        sectionTotal += board[x][y];
+        int dist = 0;
+        bfsQueue.add(new Node(x, y, 0));
         visited[x][y] = true;
 
         while (!bfsQueue.isEmpty()) {
-            Nation target = bfsQueue.poll();
+            Node target = bfsQueue.poll();
 
             for (int i=0; i<4; i++) {
                 int nextX = target.x + dx[i];
@@ -79,22 +69,16 @@ public class 인구이동_16234 {
                 if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
                 if (visited[nextX][nextY]) continue;
 
-                // 합치는 대상 노드 선별
-                int valueDiff = Math.abs(board[target.x][target.y] - board[nextX][nextY]);
-                if (valueDiff >= L && valueDiff <= R) {
-                    Nation nextNation = new Nation(nextX, nextY);
-                    bfsQueue.add(nextNation);
-                    targetNations.add(nextNation);
-                    visited[nextX][nextY] = true;
-                    sectionTotal += board[nextX][nextY];
-                }
+                visited[nextX][nextY] = true;
+                bfsQueue.add(new Node(nextX, nextY, ))
+
             }
 
         }
 
         // 아무 노드도 추가되지 않았으면 합칠 대상 없으므로 큐 비우기
-        if (targetNations.size() == 1) {
-            targetNations.poll();
+        if (targetNodes.size() == 1) {
+            targetNodes.poll();
         }
 
         return sectionTotal;
@@ -106,13 +90,15 @@ public class 인구이동_16234 {
         System.out.println(sb);
     }
 
-    public static class Nation {
+    public static class Node {
         int x;
         int y;
+        int dist;
 
-        public Nation(int x, int y) {
+        public Node(int x, int y, int dist) {
             this.x = x;
             this.y = y;
+            this.dist = dist;
         }
     }
 
